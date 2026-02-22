415dc2fcdb9f24c2d6a351b6459403d9c35c4a96
rakeshbasanidevops <cloudwithrakesh@gmail.com>
2026-02-22 19:35:14 +0000

    Update main.py_demo

--- .github/workflows/app/main.py
+++ .github/workflows/app/main.py
@@ -1,5 +1,5 @@
-def hello():
+def hello Mr.Rakesh():
     return "Hello!!! from GitHub Actions Demo by Devops Champ!!!"
 
 if __name__ == "__main__":
-    print(hello())
+    print(hello Mr.Rakesh())
